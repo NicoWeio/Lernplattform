4a478317124f042448f33a585607121af8cef10a
Nicolai Weitkemper <nico.weio@gmail.com>
2018-04-15 16:52:58 +0200

    minimale Phase -2
    (und kleine Verschönerung)

--- lernen2.nicolaiweitkemper.de/Backend/vok-answer.php
+++ lernen2.nicolaiweitkemper.de/Backend/vok-answer.php
@@ -6,17 +6,16 @@ $username = $_GET["username"];
 //$lektion = "21 Unidad 6 ¡Bienvenidos a México! - ¡Vamos!";
 $lektion = $_GET["level"];
 
-$string = file_get_contents("Userdata/".$username.".json");
-$json = json_decode($string, true);
+$json = json_decode(file_get_contents("Userdata/".$username.".json"), true);
 
 if (isset($json["data"][$_GET["course"]][$_GET["level"]][$_GET["spa"]])) {
 
-$json["data"][$_GET["course"]][$_GET["level"]][$_GET["spa"]]["total"]++;
-if ($_GET["correct"] == "true") $json["data"][$_GET["course"]][$_GET["level"]][$_GET["spa"]]["successful"]++;
-else {
-  if (isset($json["data"][$_GET["course"]][$_GET["level"]][$_GET["spa"]]["errors"][$_GET["input"]])) $json["data"][$_GET["course"]][$_GET["level"]][$_GET["spa"]]["errors"][$_GET["input"]]++;
-  else $json["data"][$_GET["course"]][$_GET["level"]][$_GET["spa"]]["errors"][$_GET["input"]] = 1;
-}
+  $json["data"][$_GET["course"]][$_GET["level"]][$_GET["spa"]]["total"]++;
+  if ($_GET["correct"] == "true") $json["data"][$_GET["course"]][$_GET["level"]][$_GET["spa"]]["successful"]++;
+  else {
+    if (isset($json["data"][$_GET["course"]][$_GET["level"]][$_GET["spa"]]["errors"][$_GET["input"]])) $json["data"][$_GET["course"]][$_GET["level"]][$_GET["spa"]]["errors"][$_GET["input"]]++;
+    else $json["data"][$_GET["course"]][$_GET["level"]][$_GET["spa"]]["errors"][$_GET["input"]] = 1;
+  }
 
 }
 else {
@@ -35,8 +34,9 @@ if ($_GET["correct"] == "true") $json["data"][$_GET["course"]][$_GET["level"]][$
 else $json["data"][$_GET["course"]][$_GET["level"]][$_GET["spa"]]["lauf"] = 0;
 
 
+//Phase:
 if (!isset($json["data"][$_GET["course"]][$_GET["level"]][$_GET["spa"]]["phase"])) $json["data"][$_GET["course"]][$_GET["level"]][$_GET["spa"]]["phase"] = ($_GET["correct"] == "true") ? 1 : 0;
-$json["data"][$_GET["course"]][$_GET["level"]][$_GET["spa"]]["phase"] += ($_GET["correct"] == "true") ? 1 : -1;
+$json["data"][$_GET["course"]][$_GET["level"]][$_GET["spa"]]["phase"] += ($_GET["correct"] == "true") ? 1 : ($json["data"][$_GET["course"]][$_GET["level"]][$_GET["spa"]]["phase"] > -2 ? -1 : 0);
 
 
 
